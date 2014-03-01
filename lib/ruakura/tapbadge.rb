3987b387c23ff770e5ce689126b973c659168c18
Phil Murray <pmurray@nevada.net.nz>
2014-03-01 22:18:24 +1300

    Left this out of the last commit

--- lib/ruakura/tapbadge.rb
+++ lib/ruakura/tapbadge.rb
@@ -1,7 +1,47 @@
+require 'tempfile'
+
+require 'prawn'
+require 'prawn/measurement_extensions'
+
+require 'active_support/core_ext/module/attribute_accessors.rb'
+require 'active_support/number_helper.rb'
+
+
 require "ruakura/tapbadge/version"
+require "ruakura/tapbadge/template"
+require "ruakura/tapbadge/generator"
 
 module Ruakura
   module Tapbadge
+    
+    BadgeCount = 2
+    TemplateRadius = 82.mm
+    Gutter = 5.mm
+    
+    FontSizes = {
+      :name   => 23,
+      :style  => 11,
+      :prices => 23
+    }
+    
+    mattr_accessor :font_file
+    
+  
     # Your code goes here...
+    def self.badge_for(options)
+      file = options.delete(:file)
+            
+      pdf = Generator.new(options).pdf
+      
+      if file
+          fh = file.kind_of?(String) ? File.open(file, 'w') : Tempfile.new("ruakura-badge")
+          fh.write(pdf)
+          fh.rewind
+          fh
+      else
+        pdf
+      end
+    end
+    
   end
 end
